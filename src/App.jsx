bcf0c780e67e7ad12c72856fe31f3004e2e4ef25
Rizakura <92446375+Rizakura0110@users.noreply.github.com>
2022-01-17 21:09:22 +0900

    再レンダリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("さししょ");
+  const [num, setNum] = useState(0);
+  const [stringShowFlag, setStringShowFlag] = useState(true);
+
   const onCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setStringShowFlag(!stringShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      stringShowFlag || setStringShowFlag(true);
+    } else {
+      stringShowFlag && setStringShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんちには</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {stringShowFlag && <p>sssssß</p>}
     </>
   );
 };
